cc955f31c8f0912ec6ceae4a664438065cdbf743
Vihar <viharshah222@gmail.com>
2022-11-22 14:11:40 +0530

    added diectory and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import categories from './components/category-menu/category-menu-items.json';
+import Directory from './components/directory-container/directory-container.component';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories = {categories}/>
   );
 }
 
